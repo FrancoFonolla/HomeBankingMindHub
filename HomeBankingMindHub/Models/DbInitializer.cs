bae68071ae67815f6f05ca0a73b09a217a24924b
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-02 10:56:02 -0300

    Agregar archivos de proyecto.

---
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class DbInitializer
+    {
+        public static void Initilize(HomeBankingContext context)
+        {
+            if(context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client{ Email="francofonolla@gmail.com", FirstName="Franco",LastName="Fonolla",Password="141516",
+                    }
+                };
+                context.Clients.AddRange(clients);
+                //guardamos
+                context.SaveChanges();
+            }
+        }
+    }
+}
