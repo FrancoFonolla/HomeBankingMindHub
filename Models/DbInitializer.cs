6ae9e9494d371d81926fc183dad228b4afbedef1
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-05 08:54:32 -0300

    Creacion de Controllers y DTOs

--- Models/DbInitializer.cs
+++ Models/DbInitializer.cs
@@ -23,17 +23,9 @@
                     context.SaveChanges();
                 }
             }
-            if(context.Clients.Any())
-            {
-                var clients = new Client[]
-                {
-                    new Client{ Email="francofonolla@gmail.com", FirstName="Franco",LastName="Fonolla",Password="141516",
-                    }
-                };
-                context.Clients.AddRange(clients);
-                //guardamos
-                context.SaveChanges();
+           
+                
             }
         }
     }
-}
+
