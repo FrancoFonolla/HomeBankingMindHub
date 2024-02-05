99ccf98a301c1ef14c31e37f568e3b71276afb35
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-05 11:12:58 -0300

    Modificaciones de Program para iniciar controladores

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 using HomeBankingMindHub.Models;
 using HomeBankingMindHub.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Add controllers to container
+builder.Services.AddControllers().AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 // Add services to the container.
+
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<HomeBankingContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("HomeBankingConexion")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
 var app = builder.Build();
 using (var scope= app.Services.CreateScope())
@@ -28,6 +36,12 @@ using (var scope= app.Services.CreateScope())
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+}
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
 app.UseStaticFiles();
 
