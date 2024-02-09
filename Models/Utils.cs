979e3b2e66bd03486aa96cdac28ca26a1b2d02cc
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-09 13:38:21 -0300

    Creación clase Utils

---
+++ Models/Utils.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class Utils
+    {
+        public string GeneratedRandomVIN()
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(100000, 999999);
+            return "VIN-" + randomNumber;
+        }
+    }
+}
