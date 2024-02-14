02d157027ad0aa654f80768c988f9e2b9036fa90
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-14 09:52:35 -0300

    Creacion repositorio Card

---
+++ Repositories/ICardRepository.cs
@@ -0,0 +1,11 @@
+﻿using HomeBankingMindHub.Models;
+
+namespace HomeBankingMindHub.Repositories
+{
+    public interface ICardRepository
+    {
+        void Save(Card card);
+        IEnumerable<Card> GetCardsByClient(long clienttId);
+
+    }
+}
---
+++ Repositories/Implements/CardRepository.cs
@@ -0,0 +1,21 @@
+﻿using HomeBankingMindHub.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeBankingMindHub.Repositories.Implements
+{
+    public class CardRepository : RepositoryBase<Card>, ICardRepository
+    {
+        public CardRepository(HomeBankingContext repositoryContext) : base(repositoryContext) { }
+
+        public void Save(Card card)
+        {
+            Create(card);
+            SaveChanges();
+        }
+        public IEnumerable<Card> GetCardsByClient(long clientId)
+        {
+            return FindByCondition(card=> card.ClientId == clientId)
+            .ToList();
+        }
+    }
+}
