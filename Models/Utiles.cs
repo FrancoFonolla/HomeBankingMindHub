6fe1bdf36b8048f2752dbb4cca4849ab0b198c2b
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-14 11:42:27 -0300

    Metodos para generar numeros de tarjeta

--- Models/Utiles.cs
+++ Models/Utiles.cs
@@ -1,4 +1,6 @@
-﻿namespace HomeBankingMindHub.Models
+﻿using System.Text;
+
+namespace HomeBankingMindHub.Models
 
 {
     public class Utiles
@@ -6,8 +8,25 @@
         public  string GeneratedRandomVIN()
         {
             Random random = new Random();
-            int randomNumber = random.Next(0, 100000000);
+            string randomNumber = random.Next(0, 100000000).ToString("D8");
+
             return ("VIN-" + randomNumber);
         }
+        public string GenerateRandomCardNumber()
+        {
+            var random = new Random();
+            var number = string.Join("-", Enumerable.Range(0, 4)
+                .Select(_ => random.Next(10000))
+                .Select(n => n.ToString("0000")));
+            Console.WriteLine(number);
+            return number;
+        }
+        public int GenerateRandomCardCvv() {
+            var random = new Random();
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("{0:D3}",random.Next(0,1000));
+            return Int32.Parse(sb.ToString().Trim());
+        }
     }
+    
 }
