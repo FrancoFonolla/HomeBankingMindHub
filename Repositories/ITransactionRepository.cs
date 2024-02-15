cf744ac72f8684b006295a4f367efd4dbc6767fd
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-15 09:18:43 -0300

    Creacion Transaction Repository

---
+++ Repositories/ITransactionRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Transactions;
+using Transaction = HomeBankingMindHub.Models.Transaction;
+namespace HomeBankingMindHub.Repositories
+{
+    public interface ITransactionRepository
+    {
+        void Save(Transaction transaction);
+        Transaction FindByNumber(long id);
+
+    }
+}
