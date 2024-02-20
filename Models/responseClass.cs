ab61fb8ed8edc15e4f1d4e19c82b91d67c1ed777
FrancoFonolla <116688690+FonoNow@users.noreply.github.com>
2024-02-20 14:05:46 -0300

    Creacion responseClass

---
+++ Models/responseClass.cs
@@ -0,0 +1,15 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class responseClass<T>
+    {
+        public T Object { get; set; }
+        public string message { get; set; }
+        public responseClass(T Object,string mess, int code)
+        {
+            this.Object = Object;
+            message = mess;
+            this.code = code;
+        }
+        public int code { get; set; }
+    }
+}
